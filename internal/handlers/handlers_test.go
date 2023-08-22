b78d2c98f4c0806cd2c68da0cd7d35c6754e2145
50u7h <50u7h@protonmail.com>
2023-08-22 17:58:17 +0300

    Writing tests for our POST handlers

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -59,21 +60,21 @@ func TestHandlers(t *testing.T) {
 				t.Errorf("for %s expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
 			}
 		} else {
-			/*
-				values := url.Values{}
-				for _, x := range e.params {
-					values.Add(x.key, x.value)
-				}
 
-				resp, err := ts.Client().PostForm(ts.URL+e.url, values)
-				if err != nil {
-					t.Log(err)
-					t.Fatal(err)
-				}
+			values := url.Values{}
+			for _, x := range e.params {
+				values.Add(x.key, x.value)
+			}
 
-				if resp.StatusCode != e.expectedStatusCode {
-					t.Errorf("for %s expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
-				}*/
+			resp, err := ts.Client().PostForm(ts.URL+e.url, values)
+			if err != nil {
+				t.Log(err)
+				t.Fatal(err)
+			}
+
+			if resp.StatusCode != e.expectedStatusCode {
+				t.Errorf("for %s expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+			}
 		}
 	}
 }
