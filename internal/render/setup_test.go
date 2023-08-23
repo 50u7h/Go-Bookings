50b87e3f7d0d6d4ab3c00d0707fd75340e1ccf69
50u7h <50u7h@protonmail.com>
2023-08-23 16:52:06 +0300

    Updating tests

--- internal/render/setup_test.go
+++ internal/render/setup_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/50u7h/Go-Bookings/internal/config"
 	"github.com/50u7h/Go-Bookings/internal/models"
 	"github.com/alexedwards/scs/v2"
+	"log"
 	"net/http"
 	"os"
 	"testing"
@@ -21,6 +22,12 @@ func TestMain(m *testing.M) {
 	// change this to true when in production
 	testApp.InProduction = false
 
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+	
 	// set up the session
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
