64a467afe307c9380bbbffe850ab2934622aab88
50u7h <50u7h@protonmail.com>
2023-08-30 17:45:33 +0300

    Connecting search availablity to the make reservation page

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -10,6 +10,7 @@ import (
 	"github.com/50u7h/Go-Bookings/internal/render"
 	"github.com/50u7h/Go-Bookings/internal/repository"
 	"github.com/50u7h/Go-Bookings/internal/repository/dbrepo"
+	"github.com/go-chi/chi/v5"
 	"strconv"
 	"time"
 
@@ -244,4 +245,26 @@ func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) 
 	render.Template(w, r, "reservation-summary.page.tmpl", &models.TemplateData{
 		Data: data,
 	})
+}
+
+func (m *Repository) ChooseRoom(w http.ResponseWriter, r *http.Request) {
+	roomID, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	m.App.Session.Get(r.Context(), "reservation")
+
+	res, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
+	if !ok {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	res.RoomID = roomID
+
+	m.App.Session.Put(r.Context(), "reservation", res)
+
+	http.Redirect(w, r, "/make-reservation", http.StatusSeeOther)
 }
