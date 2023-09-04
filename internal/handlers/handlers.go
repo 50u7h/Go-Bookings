18f0954a235fc46247bf563819c1eb8e1cdd05df
50u7h <50u7h@protonmail.com>
2023-09-04 16:58:22 +0300

    Sending formatted email using Foundation

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -188,10 +188,11 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 `, reservation.FirstName, reservation.LastName, reservation.StartDate.Format("2006-01-02"), reservation.EndDate.Format("2006-01-02"))
 
 	msg := models.MailData{
-		To:      reservation.Email,
-		From:    "bookin@go.com",
-		Subject: "Reservation Confirmation",
-		Content: htmlMessage,
+		To:       reservation.Email,
+		From:     "bookin@go.com",
+		Subject:  "Reservation Confirmation",
+		Content:  htmlMessage,
+		Template: "basic.html",
 	}
 	m.App.MailChan <- msg
 
